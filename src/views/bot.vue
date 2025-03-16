9ff0500f0c46c69147373ba62134fa0a210bd562
ldeng <ldeng@ldeng.cc>
2025-03-17 00:14:49 +0800

    Resize mobile page font

--- src/views/bot.vue
+++ src/views/bot.vue
@@ -2,7 +2,7 @@
   <div class="container mx-auto flex flex-col items-center justify-center pt-80"
         :class="[isDarkMode ? 'text-black' : 'text-white']">
     <div class="text-center p-30 rounded-lg max-w-8xl w-full bg-opacity-80">
-      <div class="pt-5 text-7xl lg:text-9xl font-light opacity-0 animate-fade-in transition-colors duration-500" style="animation-delay: 1000ms;">COMMING SOON</div>
+      <div class="pt-5 text-3xl lg:text-9xl font-light opacity-0 animate-fade-in transition-colors duration-500" style="animation-delay: 1000ms;">COMMING SOON</div>
     </div>
   </div>
 </template>
